e08eef43e9adfef86c5e468a6cf227bbff929f55
Tylor Steinberger <tlsteinberger167@gmail.com>
2025-10-15 17:35:24 -0400

    feat: allow configuring stream conversions a bit more

--- packages/effect/src/typed/fx/stream.ts
+++ packages/effect/src/typed/fx/stream.ts
@@ -6,20 +6,29 @@ import { make } from "./constructors/make.ts"
 import type * as Fx from "./Fx.ts"
 import * as Sink from "./sink/Sink.ts"
 
-export const toStream = <A, E, R>(fx: Fx.Fx<A, E, R>): Stream.Stream<A, E, R> =>
-  Stream.callback<A, E, R>((queue) =>
-    fx.run(Sink.make(
-      (cause) => Queue.failCause(queue, cause),
-      (value) => Queue.offer(queue, value)
-    ))
+export type ToStreamOptions = Parameters<typeof Stream.callback<unknown, unknown, unknown>>[1]
+
+export const toStream = <A, E, R>(fx: Fx.Fx<A, E, R>, options?: ToStreamOptions | undefined): Stream.Stream<A, E, R> =>
+  Stream.callback<A, E, R>(
+    (queue) =>
+      fx.run(Sink.make(
+        (cause) => Queue.failCause(queue, cause),
+        (value) => Queue.offer(queue, value)
+      )),
+    options
   )
 
-export const fromStream = <A, E, R>(stream: Stream.Stream<A, E, R>): Fx.Fx<A, E, R> =>
+export type FromStreamOptions = Parameters<typeof Stream.mapEffect<unknown, unknown, unknown, unknown>>[1]
+
+export const fromStream = <A, E, R>(
+  stream: Stream.Stream<A, E, R>,
+  options?: FromStreamOptions | undefined
+): Fx.Fx<A, E, R> =>
   make<A, E, R>(
     <RSink = never>(sink: Sink.Sink<A, E, RSink>): Effect.Effect<unknown, never, R | RSink> =>
       pipe(
         stream,
-        Stream.mapEffect(sink.onSuccess),
+        Stream.mapEffect(sink.onSuccess, options),
         Stream.catchCause((cause) => Stream.fromEffect(sink.onFailure(cause))),
         Stream.runDrain
       )
