e5567dad0fd0993619e9a49c925e12bb5c6b78d5
Tylor Steinberger <tlsteinberger167@gmail.com>
2025-12-10 17:23:12 -0500

    fix: more ts import fixes

--- packages/effect/src/typed/id/Uuid4.ts
+++ packages/effect/src/typed/id/Uuid4.ts
@@ -1,5 +1,5 @@
-import * as Effect from "effect/Effect"
-import * as Schema from "effect/schema/Schema"
+import * as Effect from "../../Effect.ts"
+import * as Schema from "../../schema/Schema.ts"
 import { uuidStringify } from "./_uuid-stringify.ts"
 import { RandomValues } from "./RandomValues.ts"
 
